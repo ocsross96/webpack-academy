a39683d3571f706b302ff1ca710b820deeaa7a5f
Ross Matthews <ross.matthews@ratedpeople.com>
2017-12-18 16:02:19 +0000

    Core concepts. Part 8. Loaders and rules. Added loader options and maintain consistent style.

--- webpack-academy-one/webpack.config.js
+++ webpack-academy-one/webpack.config.js
@@ -1,6 +1,41 @@
+const path = require('path');
+
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js"
+    filename: "bundle.js",
+    path: path.join(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: { 
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { 
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test: /\.jpeg$/,
+        use: [
+          {
+            loader: "url-loader", 
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
   }
 }
