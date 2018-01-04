c5d2c128781dcd4f0f54fb892828a103cd0f9251
Ross Matthews <ross.matthews@ratedpeople.com>
2018-01-04 17:27:26 +0000

    Added essential config

--- web-fundamentals/starting-out-right/build-utils/webpack.common.js
+++ web-fundamentals/starting-out-right/build-utils/webpack.common.js
@@ -1,11 +1,32 @@
 const commonPaths = require("./common-paths");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
   entry: "./src/",
   output: {
-    filename: "bundle.js",
+    filename: "[chunkhash].bundle.js",
     path: commonPaths.outputPath
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.png/,
+        use: [
+          { 
+            loader: "url-loader",
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin()
+  ]
 };
 
 module.exports = config;
