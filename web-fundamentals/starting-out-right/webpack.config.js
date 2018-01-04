ba33bb5b7f333e6c01affe7734c9ff7163ba8f3a
Ross Matthews <ross.matthews@ratedpeople.com>
2018-01-04 09:56:15 +0000

    Work on add ons

--- web-fundamentals/starting-out-right/webpack.config.js
+++ web-fundamentals/starting-out-right/webpack.config.js
@@ -1,10 +1,20 @@
 const commonConfig = require("./build-utils/webpack.common");
 const webpackMerge = require("webpack-merge");
 
+const addons = (addonsArg) => {
+  let addons = []
+    .concat.apply([], [addonsArg]) // Normalize array of addons (flatten)
+    .filter(Boolean);              // If addons is undefined, filter it out
+  
+  return addons.map((addonName) => require(`./build-utils/addons/webpack.${addonName}.js`));
+}
 
 module.exports = (env) => {
   console.log(env);
   const envConfig = require(`./build-utils/webpack.${env.env}.js`);
+  const mergedConfig = webpackMerge(commonConfig, envConfig, ...addons(env.addons));
 
-  return webpackMerge(commonConfig, envConfig);
+  console.log(mergedConfig);
+
+  return mergedConfig;
 }
