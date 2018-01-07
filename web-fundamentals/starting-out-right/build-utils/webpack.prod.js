e5200c346bd154a91c35b16172bca1e266914a9f
Ross Matthews <ross.matthews@ratedpeople.com>
2018-01-07 10:38:50 +0000

    Final commit with all production plug-ins

--- web-fundamentals/starting-out-right/build-utils/webpack.prod.js
+++ web-fundamentals/starting-out-right/build-utils/webpack.prod.js
@@ -1,7 +1,31 @@
 const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+const UglifyJsWebpackPlugin = require("uglifyjs-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+
 module.exports = {
   devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ExtractTextWebpackPlugin.extract({
+          use: "css-loader",
+          fallback: "style-loader"
+        })
+      }
+    ]
+  },
   plugins: [
-    new ExtractTextWebpackPlugin("style.css")
+    new ExtractTextWebpackPlugin("style.css"),
+    new UglifyJsWebpackPlugin({
+      sourceMap: true
+    }),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.(js|html|css)$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
   ]
-}
+};
